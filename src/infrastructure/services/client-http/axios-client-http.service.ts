2058640499d8fd4ecb0397e441e2369d7687cdfb
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-16 16:31:20 -0300

    ✨ feat: add axios http client service

---
+++ src/infrastructure/services/client-http/axios-client-http.service.ts
@@ -0,0 +1,23 @@
+import type { AxiosInstance } from "axios";
+import type { IHttpClientService } from "src/domain/services/client-http";
+import type { CustomHttpRequest, CustomHttpResponse } from "src/domain/types";
+
+export class AxiosClientHttpService implements IHttpClientService {
+  constructor(private readonly _axiosInstance: AxiosInstance) {}
+
+  async request<T = unknown>(
+    data: CustomHttpRequest
+  ): Promise<CustomHttpResponse<T>> {
+    // TODO: Add try catch
+    const response = await this._axiosInstance.request({
+      url: data.url,
+      method: data.method,
+      data: data.body,
+    });
+
+    return {
+      statusCode: response.status,
+      body: response.data,
+    };
+  }
+}
