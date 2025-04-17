706648815e83b5111dde5116f154db5ed6ce14e9
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-17 12:12:26 -0300

    ✨ feat: add new envs | MONGODB_URI and DOCUMENT_OBJECTID

--- configs/environment/env.ts
+++ configs/environment/env.ts
@@ -15,12 +15,14 @@ if (result.error) {
 }
 
 const envSchema = z.object({
+  DOCUMENT_OBJECTID: z.string(),
+  EXTERNAL_API: z.string(),
+  IMAGE_DOLLAR_PATH: z.string().default("./assets/images/dollar.jpg"),
+  MONGODB_URI: z.string(),
   TWITTER_APP_KEY: z.string(),
   TWITTER_APP_SECRET: z.string(),
   TWITTER_ACCESS_TOKEN: z.string(),
   TWITTER_ACCESS_SECRET: z.string(),
-  EXTERNAL_API: z.string(),
-  IMAGE_DOLLAR_PATH: z.string().default("./assets/images/dollar.jpg"),
 });
 
 const env = envSchema.parse(process.env, {
@@ -30,8 +32,10 @@ const env = envSchema.parse(process.env, {
 });
 
 const {
+  DOCUMENT_OBJECTID,
   EXTERNAL_API,
   IMAGE_DOLLAR_PATH,
+  MONGODB_URI,
   TWITTER_ACCESS_TOKEN,
   TWITTER_ACCESS_SECRET,
   TWITTER_APP_KEY,
@@ -39,8 +43,10 @@ const {
 } = env;
 
 export {
+  DOCUMENT_OBJECTID,
   EXTERNAL_API,
   IMAGE_DOLLAR_PATH,
+  MONGODB_URI,
   TWITTER_ACCESS_SECRET,
   TWITTER_ACCESS_TOKEN,
   TWITTER_APP_KEY,
