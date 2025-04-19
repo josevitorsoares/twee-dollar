bcb4aa94b29f027f4c17d2fdb9cf1eaace9c7774
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-18 22:38:07 -0300

    🧹 cleanup: remove generic database repository structure

--- src/domain/factories/repositories/mongodb/mongodb.repository.factory.ts
+++
@@ -1,8 +0,0 @@
-import type { IDatabaseRepository } from "src/domain/repositories";
-import { MongoDBRepository } from "src/infrastructure";
-
-export const makeMongoDBRepository = (): IDatabaseRepository => {
-  const mongoDbRepository = new MongoDBRepository();
-
-  return mongoDbRepository;
-};
