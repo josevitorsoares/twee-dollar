f0bf2a50f72faaa28e067d11970d4a1d0e42f644
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-19 00:10:39 -0300

    ✨ feat: add close database connection

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+import { closeDatabase } from "src/infrastructure/database";
 import { makeGetDollarUseCase } from "./src/domain/factories";
 
 const getDollarUseCase = makeGetDollarUseCase();
 
-getDollarUseCase.execute();
+getDollarUseCase.execute().finally(async () => await closeDatabase());
